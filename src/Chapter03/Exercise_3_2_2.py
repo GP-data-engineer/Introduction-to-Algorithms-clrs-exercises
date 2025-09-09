ea5d2136b601dfbf03cab9931092c331980cfc31
Grzegorz Pieniak <gp79@tutamail.com>
2025-09-09 22:22:31 +0200

    add Exercise_3_2_2.py | test_exercise_3_2_2.py

---
+++ src/Chapter03/Exercise_3_2_2.py
@@ -0,0 +1,9 @@
+﻿# Exercise 3.2-2
+# Prove formula (3.16) - here we assume it's a known logarithmic identity.
+# Example: log_a(bc) = log_a(b) + log_a(c)
+
+import math
+
+def log_product(a, b, base):
+    """Return log_base(a*b) and verify formula log(a*b) = log(a) + log(b)."""
+    return math.log(a*b, base), math.log(a, base) + math.log(b, base)
