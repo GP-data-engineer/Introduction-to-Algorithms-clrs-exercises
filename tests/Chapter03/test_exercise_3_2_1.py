7d6fca274872d4ea16758e8aca8dd7bc441cc231
Grzegorz Pieniak <gp79@tutamail.com>
2025-09-09 22:20:03 +0200

    add modiyfi Exercise_3_2_1.py | test_exercise_3_2_1.py

--- tests/Chapter03/test_exercise_3_2_1.py
+++ tests/Chapter03/test_exercise_3_2_1.py
@@ -1,12 +1,9 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from Exercise_3_2_1 import is_monotonic_increasing, sum_of_functions, product_of_functions
 
-import pytest
-from src.Chapter03.Exercise_3_2_1 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_sum_and_product_monotonic():
+    f = lambda n: n
+    g = lambda n: n**2
+    n_values = list(range(1, 10))
+    assert is_monotonic_increasing(sum_of_functions(f, g, n_values))
+    assert is_monotonic_increasing(product_of_functions(f, g, n_values))
