f8c7bcbfe197c4365409ba286960778612f5144f
Grzegorz Pieniak <gp79@tutamail.com>
2025-09-15 21:18:52 +0200

    Modified test_exercise_4_2_2.py & test_exercise_4_2_2.py

--- tests/Chapter04/test_exercise_4_2_2.py
+++ tests/Chapter04/test_exercise_4_2_2.py
@@ -1,12 +1,19 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿"""
+Unit tests for Exercise 4.2-2 (Strassen's algorithm for n x n matrices).
+"""
 
 import pytest
-from src.Chapter04.Exercise_4_2_2 import solution_function
+from src.Chapter04.Exercise_4_2_2 import strassen
 
-def test_basic_case():
-    assert solution_function() is None
 
-def test_additional_case():
-    assert True
+def test_strassen_2x2():
+    A = [[1, 2], [3, 4]]
+    B = [[5, 6], [7, 8]]
+    expected = [[19, 22], [43, 50]]
+    assert strassen(A, B) == expected
+
+
+def test_strassen_identity():
+    A = [[2, 3], [4, 5]]
+    I = [[1, 0], [0, 1]]
+    assert strassen(A, I) == A
