72a14785b559c8ae26be47c71b0f333a2c0f1335
Grzegorz Pieniak <gp79@tutamail.com>
2025-10-07 22:02:09 +0200

    new file:   src/Chapter06/Exercise_6_2_5.py 	new file:   tests/Chapter06/test_exercise_6_2_5.py

---
+++ src/Chapter06/Exercise_6_2_5.py
@@ -0,0 +1,26 @@
+﻿"""
+Exercise 6.2-5
+English: Write an iterative version of MAX-HEAPIFY.
+Polish: Napisz iteracyjną wersję MAX-HEAPIFY.
+"""
+
+def max_heapify_iter(A, i, n):
+    while True:
+        l = 2*i+1
+        r = 2*i+2
+        largest = i
+        if l < n and A[l] > A[largest]:
+            largest = l
+        if r < n and A[r] > A[largest]:
+            largest = r
+        if largest != i:
+            A[i], A[largest] = A[largest], A[i]
+            i = largest
+        else:
+            break
+    return A
+
+if __name__ == "__main__":
+    arr = [3, 17, 10, 1, 5]
+    print("Exercise 6.2-5 Result:")
+    print(max_heapify_iter(arr, 0, len(arr)))
---
+++ tests/Chapter06/test_exercise_6_2_5.py
@@ -0,0 +1,7 @@
+﻿import pytest
+from src.Chapter06.Exercise_6_2_5 import max_heapify_iter
+
+def test_max_heapify_iter():
+    arr = [3, 17, 10, 1, 5]
+    result = max_heapify_iter(arr, 0, len(arr))
+    assert result[0] == 17
