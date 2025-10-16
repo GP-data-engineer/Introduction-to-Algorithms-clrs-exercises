4871c950c44dbdbf79a804ac9895e7c36c213656
Grzegorz Pieniak <gp79@tutamail.com>
2025-10-16 14:57:59 +0200

    modified:   tests/Chapter07/test_exercise_7_2_1.py

--- tests/Chapter07/test_exercise_7_2_1.py
+++ tests/Chapter07/test_exercise_7_2_1.py
@@ -1,12 +1,5 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest 
+from src.Chapter07.Exercise_7_2_1 import recurrence_solution
 
-import pytest
-from src.Chapter07.Exercise_7_2_1 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_recurrence_solution():
+    assert "Θ(n^2)" in recurrence_solution()
