6c2aab0675711cddddd2cd8e51a586ba1e5d8e03
Grzegorz Pieniak <gp79@tutamail.com>
2025-12-03 20:12:25 +0100

    modified:   tests/Chapter09/test_exercise_9_3_6.py

--- tests/Chapter09/test_exercise_9_3_6.py
+++ tests/Chapter09/test_exercise_9_3_6.py
@@ -1,12 +1,12 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿# Test for Exercise 9.3.6 (EN): Verifies partitioning around known i-th smallest element.
+# Test do Exercise 9.3.6 (PL): Sprawdza podział względem znanego i-tego najmniejszego elementu.
 
 import pytest
-from src.Chapter09.Exercise_9_3_6 import solution_function
+from src.Chapter09.Exercise_9_3_6 import partition_by_known_element
 
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_partition_correctness():
+    A = [3, 2, 9, 0, 7, 5, 4, 8, 6, 1]
+    pivot = sorted(A)[4]  # 5th smallest
+    smaller, equal, greater = partition_by_known_element(A, pivot)
+    assert all(x < pivot for x in smaller)
+    assert all(x > pivot for x in greater)
