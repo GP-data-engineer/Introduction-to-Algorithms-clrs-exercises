06ac8e7944f5d4aceb09219d771eb94a68f17f77
Grzegorz Pieniak <gp79@tutamail.com>
2026-01-21 20:22:59 +0100

    modified:   tests/Chapter10/test_exercise_10_3_1.py

--- tests/Chapter10/test_exercise_10_3_1.py
+++ tests/Chapter10/test_exercise_10_3_1.py
@@ -1,12 +1,15 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿from src.Chapter08.Exercise_10_3_1 import multitable_representation, single_table_representation
 
-import pytest
-from src.Chapter10.Exercise_10_3_1 import solution_function
+def test_multitable_structure():
+    seq = [1, 2, 3]
+    rep = multitable_representation(seq)
+    assert rep["key"] == [1, 2, 3]
+    assert rep["prev"] == [None, 0, 1]
+    assert rep["next"] == [1, 2, None]
 
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_single_table_structure():
+    seq = [10, 20]
+    rep = single_table_representation(seq)
+    assert rep[0]["key"] == 10
+    assert rep[0]["next"] == 1
+    assert rep[1]["prev"] == 0
