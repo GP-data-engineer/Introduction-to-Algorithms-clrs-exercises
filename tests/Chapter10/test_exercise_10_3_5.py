53c6fdc36b52aef8e9537e7ff2206e942110d3b4
Grzegorz Pieniak <gp79@tutamail.com>
2026-01-28 20:19:41 +0100

    modified:   tests/Chapter10/test_exercise_10_3_5.py

--- tests/Chapter10/test_exercise_10_3_5.py
+++ tests/Chapter10/test_exercise_10_3_5.py
@@ -1,12 +1,8 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿from src.Chapter10.Exercise_10_3_5 import compactify_list
 
-import pytest
-from src.Chapter10.Exercise_10_3_5 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_compactify():
+    L = {"key": [1, 2], "prev": [None, 0], "next": [1, None]}
+    F = {"free": [2, 3]}
+    L2, F2 = compactify_list(L, F, 4)
+    assert F2["free"] == [2, 3]
+    assert L2["prev"] == [None, 0]
