ba5e8f10c95e6f235540f18d226d00598e9c78e1
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-15 14:14:44 +0100

    modified:   tests/Chapter11/test_exercise_11_2_2.py

--- tests/Chapter11/test_exercise_11_2_2.py
+++ tests/Chapter11/test_exercise_11_2_2.py
@@ -1,12 +1,14 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿# Exercise 11.2-2 tests
 
 import pytest
-from src.Chapter11.Exercise_11_2_2 import solution_function
+from src.Chapter11.Exercise_11_2_2 import HashTableChaining
 
-def test_basic_case():
-    assert solution_function() is None
 
-def test_additional_case():
-    assert True
+def test_hash_insert():
+    ht = HashTableChaining(9)
+    keys = [5, 28, 19]
+    for k in keys:
+        ht.insert(k)
+    assert 5 in ht.get_table()[5]
+    assert 28 in ht.get_table()[1]
+    assert 19 in ht.get_table()[1]
