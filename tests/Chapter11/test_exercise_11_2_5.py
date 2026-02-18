21d3634738a8ae949eacdb69fae7d20720eb1e28
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-18 17:41:16 +0100

    modified:   tests/Chapter11/test_exercise_11_2_5.py

--- tests/Chapter11/test_exercise_11_2_5.py
+++ tests/Chapter11/test_exercise_11_2_5.py
@@ -1,12 +1,12 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿# Exercise 11.2-5 tests
 
 import pytest
-from src.Chapter11.Exercise_11_2_5 import solution_function
+from src.Chapter11.Exercise_11_2_5 import pigeonhole_collision
 
-def test_basic_case():
-    assert solution_function() is None
 
-def test_additional_case():
-    assert True
+def test_pigeonhole_true():
+    assert pigeonhole_collision(101, 10, 10)
+
+
+def test_pigeonhole_false():
+    assert not pigeonhole_collision(50, 10, 10)
