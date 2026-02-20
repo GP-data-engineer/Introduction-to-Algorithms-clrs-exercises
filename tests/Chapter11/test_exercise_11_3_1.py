02816ebfc48942f9854cae324e7b279ef8400ea3
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-20 19:10:08 +0100

    modified:   tests/Chapter11/test_exercise_11_3_1.py

--- tests/Chapter11/test_exercise_11_3_1.py
+++ tests/Chapter11/test_exercise_11_3_1.py
@@ -1,31 +1,15 @@
-﻿# Exercise 11.3-1 (CLRS)
-# PL:
-# Mamy listę długości n, gdzie każdy element przechowuje klucz k
-# oraz wartość funkcji haszującej h(k).
-# Jak wykorzystać h(k) do przyspieszenia wyszukiwania?
-#
-# EN:
-# Given a list of length n where each element stores key k and its hash value h(k).
-# How can we use stored hash values to speed up search?
-
-class HashedList:
-
-    def __init__(self):
-        self.data = []
-
-    def insert(self, key, hash_value):
-        self.data.append((key, hash_value))
-
-    def search(self, key, hash_value):
-        # first compare hash values, then actual keys
-        for k, h in self.data:
-            if h == hash_value:  # quick rejection if hashes differ
-                if k == key:
-                    return True
-        return False
+﻿import pytest
+from src.Chapter11.Exercise_11_3_1 import HashedList
 
 
-if __name__ == "__main__":
+def test_search_found():
     hl = HashedList()
-    hl.insert("abc", hash("abc"))
-    print("Found:", hl.search("abc", hash("abc")))
+    h = hash("key")
+    hl.insert("key", h)
+    assert hl.search("key", h)
+
+
+def test_search_not_found():
+    hl = HashedList()
+    hl.insert("key", hash("key"))
+    assert not hl.search("other", hash("other"))
