62c34c17f04f8465b83f937da6d890ca563909a6
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-24 20:09:40 +0100

    modified:   tests/Chapter11/test_exercise_11_3_4.py

--- tests/Chapter11/test_exercise_11_3_4.py
+++ tests/Chapter11/test_exercise_11_3_4.py
@@ -1,12 +1,9 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_3_4 import multiplication_hash
 
-import pytest
-from src.Chapter11.Exercise_11_3_4 import solution_function
 
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_positions():
+    m = 1000
+    results = [multiplication_hash(k, m) for k in [61,62,63,64,65]]
+    assert len(results) == 5
+    assert all(0 <= r < m for r in results)
