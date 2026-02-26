32b35e4bca22284d4a50d1f6a1261a0e2139148a
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-26 18:19:04 +0100

    modified:   tests/Chapter11/test_exercise_11_3_6.py

--- tests/Chapter11/test_exercise_11_3_6.py
+++ tests/Chapter11/test_exercise_11_3_6.py
@@ -1,12 +1,12 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_3_6 import polynomial_hash
 
-import pytest
-from src.Chapter11.Exercise_11_3_6 import solution_function
 
-def test_basic_case():
-    assert solution_function() is None
+def test_polynomial_hash_basic():
+    assert polynomial_hash([1,2,3], 5, 13) == (
+        (1*1 + 2*5 + 3*25) % 13
+    )
 
-def test_additional_case():
-    assert True
+
+def test_mod_property():
+    assert polynomial_hash([0,0,0], 7, 17) == 0
