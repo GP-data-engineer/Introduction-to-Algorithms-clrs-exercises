e38c05b666cd06913a67b39d1078d20f5c5664a6
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-27 18:03:43 +0100

    modified:   tests/Chapter11/test_exercise_11_4_1.py

--- tests/Chapter11/test_exercise_11_4_1.py
+++ tests/Chapter11/test_exercise_11_4_1.py
@@ -1,12 +1,17 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_4_1 import OpenAddressingHash
 
-import pytest
-from src.Chapter11.Exercise_11_4_1 import solution_function
+def test_linear_insert():
+    h = OpenAddressingHash(11)
+    h.insert(10, "linear")
+    assert h.T[10] == 10
 
-def test_basic_case():
-    assert solution_function() is None
+def test_quadratic_insert():
+    h = OpenAddressingHash(11)
+    h.insert(10, "quadratic")
+    assert h.T[10] == 10
 
-def test_additional_case():
-    assert True
+def test_double_insert():
+    h = OpenAddressingHash(11)
+    h.insert(10, "double")
+    assert h.T[10] == 10
