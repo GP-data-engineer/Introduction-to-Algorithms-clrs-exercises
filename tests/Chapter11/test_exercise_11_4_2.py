abe5bf93e413dec5bd63cfcc32dcbbc3142b7d09
Grzegorz Pieniak <gp79@tutamail.com>
2026-02-28 06:53:11 +0100

    modified:   tests/Chapter11/test_exercise_11_4_2.py

--- tests/Chapter11/test_exercise_11_4_2.py
+++ tests/Chapter11/test_exercise_11_4_2.py
@@ -1,12 +1,10 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_4_2 import HashTableOpen, DELETED
 
-import pytest
-from src.Chapter11.Exercise_11_4_2 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_insert_and_delete():
+    h = HashTableOpen(11)
+    h.insert(10)
+    h.insert(21)
+    h.delete(10)
+    assert h.T[h.search(21)] == 21
+    assert DELETED in h.T
