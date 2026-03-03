ee176223ed137d7d5e467c5de4f106fb9c7f2e43
Grzegorz Pieniak <gp79@tutamail.com>
2026-03-03 20:36:32 +0100

    modified:   tests/Chapter11/test_exercise_11_4_4.py

--- tests/Chapter11/test_exercise_11_4_4.py
+++ tests/Chapter11/test_exercise_11_4_4.py
@@ -1,12 +1,6 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_4_4 import positions_checked
 
-import pytest
-from src.Chapter11.Exercise_11_4_4 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_positions_checked():
+    assert positions_checked(12, 4) == 3
+    assert positions_checked(11, 3) == 11
