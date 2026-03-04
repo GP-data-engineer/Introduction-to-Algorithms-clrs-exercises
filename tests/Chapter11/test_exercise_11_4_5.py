c0a91354f07a58fcfca03f9bc963d0e68958cc15
Grzegorz Pieniak <gp79@tutamail.com>
2026-03-04 19:32:13 +0100

    modified:   tests/Chapter11/test_exercise_11_4_5.py

--- tests/Chapter11/test_exercise_11_4_5.py
+++ tests/Chapter11/test_exercise_11_4_5.py
@@ -1,12 +1,6 @@
-﻿\"\"\"
-Proof or explanation (comment in English):
-\"\"\"
+﻿import pytest
+from src.Chapter11.Exercise_11_4_5 import good_alpha
 
-import pytest
-from src.Chapter11.Exercise_11_4_5 import solution_function
-
-def test_basic_case():
-    assert solution_function() is None
-
-def test_additional_case():
-    assert True
+def test_good_alpha():
+    alphas = good_alpha()
+    assert all(a < 0.5 for a in alphas)
