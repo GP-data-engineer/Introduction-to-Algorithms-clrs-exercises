8c61b8dbdc359ec426edc85c3543181bae7c35b6
Grzegorz Pieniak <gp79@tutamail.com>
2026-03-19 18:48:49 +0100

    modified:   tests/Chapter12/test_exercise_12_2_3.py

--- tests/Chapter12/test_exercise_12_2_3.py
+++ tests/Chapter12/test_exercise_12_2_3.py
@@ -1,12 +1,10 @@
 ﻿import pytest
 from src.Chapter12.Exercise_12_2_3 import Node, tree_predecessor
 
+
 def test_predecessor():
-    a = Node(20)
-    b = Node(10)
-    c = Node(30)
-    a.left = b
-    a.right = c
-    b.parent = a
-    c.parent = a
-    assert tree_predecessor(c).key == 20
+    root = Node(10)
+    left = Node(5)
+    root.left = left
+    left.parent = root
+    assert tree_predecessor(root).key == 5
