569a3be7b891e6e6fde80dcf2b7568fe698e9aee
Grzegorz Pieniak <gp79@tutamail.com>
2026-03-19 18:47:42 +0100

    modified:   tests/Chapter12/test_exercise_12_2_2.py

--- tests/Chapter12/test_exercise_12_2_2.py
+++ tests/Chapter12/test_exercise_12_2_2.py
@@ -1,7 +1,10 @@
 ﻿import pytest
 from src.Chapter12.Exercise_12_2_2 import Node, tree_minimum, tree_maximum
 
+
 def test_min_max():
-    root = Node(20, Node(10), Node(30))
-    assert tree_minimum(root).key == 10
-    assert tree_maximum(root).key == 30
+    root = Node(10)
+    root.left = Node(5)
+    root.right = Node(20)
+    assert tree_minimum(root).key == 5
+    assert tree_maximum(root).key == 20
